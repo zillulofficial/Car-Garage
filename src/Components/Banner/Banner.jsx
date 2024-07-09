e397a5b8f2169219d661784e0573c6415a01feba
zillulofficial <zillul.hakim100@gmail.com>
2024-07-09 22:47:52 +0600

    banner and about us section made responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,11 +6,11 @@ const Banner = () => {
                 <div id="slide1" className="carousel-item relative w-full ">
                     <img
                         src="https://i.postimg.cc/5ybq1Y5b/5.jpg"
-                        className="w-full h-[750px]" />
-                    <div className="absolute h-full top-0 bg-gradient-to-r from-[#151515] gap-5  w-1/3 flex items-center">
-                        <div className="pl-12 space-y-7">
-                            <h1 className="text-white text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
-                            <p className="text-white text-roboto">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        className="w-full lg:h-[750px]" />
+                    <div className="absolute h-full top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] gap-5  w-1/3 flex items-center">
+                        <div className="pl-12 space-y-4 md:space-y-7">
+                            <h1 className="text-white text-3xl lg:text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
+                            <p className="text-white text-roboto w-80">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                             <div>
                                 <button className="btn border-none mr-5 bg-[#FF3811] text-white ">Discover More</button>
                                 <button className="btn btn-outline border-white text-white">Latest Project</button>
@@ -25,11 +25,11 @@ const Banner = () => {
                 <div id="slide2" className="carousel-item relative w-full ">
                     <img
                         src="https://i.postimg.cc/fL4f2gJ3/4.jpg"
-                        className="w-full h-[750px]" />
+                        className="w-full lg:h-[750px]" />
                     <div className="absolute h-full top-0 bg-gradient-to-r from-[#151515] gap-5  w-1/3 flex items-center">
-                        <div className="pl-12 space-y-7">
-                            <h1 className="text-white text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
-                            <p className="text-white text-roboto">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="pl-12 space-y-4 md:space-y-7">
+                            <h1 className="text-white text-3xl lg:text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
+                            <p className="text-white text-roboto w-80">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                             <div>
                                 <button className="btn border-none mr-5 bg-[#FF3811] text-white ">Discover More</button>
                                 <button className="btn btn-outline border-white text-white ">Latest Project</button>
@@ -44,11 +44,11 @@ const Banner = () => {
                 <div id="slide3" className="carousel-item relative w-full ">
                     <img
                         src="https://i.postimg.cc/8z0HWCGm/3.jpg"
-                        className="w-full h-[750px]" />
+                        className="w-full lg:h-[750px]" />
                     <div className="absolute h-full top-0 bg-gradient-to-r from-[#151515] gap-5  w-1/3 flex items-center">
-                        <div className="pl-12 space-y-7">
-                            <h1 className="text-white text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
-                            <p className="text-white text-roboto">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="pl-12 space-y-4 md:space-y-7">
+                            <h1 className="text-white text-3xl lg:text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
+                            <p className="text-white text-roboto w-80">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                             <div>
                                 <button className="btn border-none mr-5 bg-[#FF3811] text-white ">Discover More</button>
                                 <button className="btn btn-outline border-white text-white ">Latest Project</button>
@@ -63,11 +63,11 @@ const Banner = () => {
                 <div id="slide4" className="carousel-item relative w-full ">
                     <img
                         src="https://i.postimg.cc/vBWLpLV0/1.jpg"
-                        className="w-full h-[750px]" />
+                        className="w-full lg:h-[750px]" />
                     <div className="absolute h-full top-0 bg-gradient-to-r from-[#151515] gap-5  w-1/3 flex items-center">
-                        <div className="pl-12 space-y-7">
-                            <h1 className="text-white text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
-                            <p className="text-white text-roboto">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div className="pl-12 space-y-4 md:space-y-7">
+                            <h1 className="text-white text-3xl lg:text-6xl font-bold text-roboto">Affordable Price For Car Servicing</h1>
+                            <p className="text-white text-roboto w-80">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                             <div>
                                 <button className="btn border-none mr-5 bg-[#FF3811] text-white ">Discover More</button>
                                 <button className="btn btn-outline border-white text-white ">Latest Project</button>
--- src/Components/Home/AboutUs.jsx
+++ src/Components/Home/AboutUs.jsx
@@ -1,20 +1,20 @@
 
 const AboutUs = () => {
     return (
-        <div className="container mx-auto flex justify-between items-center gap-14 mb-24">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-28 md:gap-14 mb-24">
             <div className="w-1/2 relative">
-                <img className="w-3/4 rounded-xl" src="https://i.postimg.cc/MHCpQ45D/person.jpg" alt="" />
-                <img className="w-1/2 rounded-xl absolute right-5 top-2/3 border-white border-8" src="https://i.postimg.cc/yxgNWBmx/parts.jpg" alt="" />
+                <img className="md:w-3/4 rounded-xl" src="https://i.postimg.cc/MHCpQ45D/person.jpg" alt="" />
+                <img className="md:w-1/2 w-3/4 rounded-xl absolute right-5 top-2/3 border-white border-8" src="https://i.postimg.cc/yxgNWBmx/parts.jpg" alt="" />
             </div>
-            <div className="w-1/2">
+            <div className="md:w-[490px] mx-auto">
                 <p className="text-[#FF3811] text-2xl font-extrabold roboto mb-5">About Us</p>
-                <p className="font-bold text-5xl mb-8">We are qualified <br className="lg:block hidden "/>& of experience <br className="lg:block hidden "/>in this field</p>
-                <p className="w-[490px] mb-5">There are many variations of passages of Lorem Ipsum available,
+                <p className="font-bold text-3xl md:text-5xl mb-8">We are qualified <br className="lg:block hidden "/>& of experience <br className="lg:block hidden "/>in this field</p>
+                <p className=" mb-5 ">There are many variations of passages of Lorem Ipsum available,
                     but the majority have suffered alteration in some form, by injected
                     humour, or randomised words which don't look even slightly believable. </p>
-                <p className="w-[490px] mb-8">the majority have suffered alteration in some form, by injected humour,
+                <p className=" mb-8 mx-auto">the majority have suffered alteration in some form, by injected humour,
                     or randomised words which don't look even slightly believable. </p>
-                    <button className="btn">Get More Info</button>
+                    <button className="btn bg-[#FF3811] text-white border-none">Get More Info</button>
             </div>
         </div>
     );
